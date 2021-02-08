129eb7c0e87522b7b182467a199e2aa765094a53
JackDaniek <kyoshiro.maruo@di-v.co.jp>
2021-02-08 17:42:14 +0900

    heroku設定

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -1,29 +1,57 @@
 function check() {
+  // 関数の宣言
   const posts = document.querySelectorAll(".post");
+  // .postと名前のついたセレクタを全て取得している
   posts.forEach(function (post) {
+    // 配列（posts）から要素をそれぞれ取り出す。要素がクリックした際に動く処理の記述を定義
     if (post.getAttribute("data-load") != null) {
+      // post要素から指定した要素から指定した属性がからではなかった時
       return null;
+      // 処理を止める
     }
+    
     post.setAttribute("data-load", "true");
+    // post要素に対して属性を追加後、既存の値を変更している
     post.addEventListener("click", () => {
+      // 要素がクリックされた時に行う処理の定義
       const postId = post.getAttribute("data-id");
+      // メモ１つ分のidを取得している
       const XHR = new XMLHttpRequest();
+      // 非同期通信をできるよう変数定義している
       XHR.open("GET", `/posts/${postId}`, true);
+      // checkedアクションへのルーティングを設定している
       XHR.responseType = "json";
+      // リクエストを送った際、返すレスポンスをjson形式で指定している
       XHR.send();
+      // リクエストをできるようにしている
       XHR.onload = () => {
+        // レスポンスが返ってきた時中の処理を呼び出す
+        
         if (XHR.status != 200) {
+          
+          // 処理が成功しなかった場合
           alert(`Error ${XHR.status}: ${XHR.statusText}`);
-          return null;          
+          // エラーが発生した場合エラーメッセージが表示されるようアラートを設定している
+          return null;
+          // ここでjavascriptの処理から抜け出す。32行以降の処理を行わないようにする          
         }
+        
         const item = XHR.response.post;
-        if (item.checked === true) {
+        // レスポンスとして返ってきたメモのレコードデータを取得している
+        if (item.checked === true){
+          
+          // 既読できた場合の処理を定義
           post.setAttribute("data-check", "true");
-        } else if (item.checked === false) {
+          // post要素に対して属性を追加後、既存の値を変更している
+        } 
+        else if (item.checked === false) {
+          // 既読できなかった場合の処理を定義
           post.removeAttribute("data-check");
+          // post要素から属性を削除
         }
       };
     });
   });
 }
 setInterval(check, 1000);
+// 既読の動作が1秒ごとに行われるよう設定している
--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,20 +1,42 @@
 function memo () {
+  // 関数の宣言
+
   const submit = document.getElementById("submit");
+  // 投稿ボタンに該当するHTMLの要素を取得し変数に代入している
   submit.addEventListener("click", (e) => {
+    // submitボタンがclickされた時に実行される処理を処理を定義している
     const formData = new FormData(document.getElementById("form"));
+    // フォームに入力されたものも含めたHTMLを要素ごと取得して変数に代入している
+
     const XHR = new XMLHttpRequest();
+    // 非同期通信を可能にするオブジェクトを作り、変数に代入する
     XHR.open("POST", "/posts", true);
+    // クリエイトアクションへのリクエストを指定する
     XHR.responseType = "json";
+    // リクエストを送った際、返すレスポンスをjson形式で指定している
     XHR.send(formData);
+    // メモ投稿フォームに入力された投稿フォームに入力された情報を送信している
+
     XHR.onload = () => {
+      // 非同期通信のレスポンスを受信成功した時に中の処理を呼び出す
+
       if (XHR.status != 200) {
+        // 処理が成功しなかった場合
         alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        // エラーが発生した場合エラーメッセージが表示されるようアラートを設定している
         return null;
+        // ここでjavascriptの処理から抜け出す。27行以降の処理を行わないようにする
       }
+
       const item = XHR.response.post;
+      // レスポンスとして返ってきたメモのレコードデータを取得している
       const list = document.getElementById("list");
+      // HTMLを描画したい親要素の中からlist要素を取得している
       const formText = document.getElementById("content");
-      const HTML = `
+      // HTMLを描画したい親要素の中からcontent要素を取得している
+      const HTML =
+      // メモとして描画する部分のHTMLを定義している
+      `
         <div class="post" data-id=${item.id}>
           <div class="post-date">
             投稿日時：${item.created_at}
@@ -23,10 +45,16 @@ function memo () {
           ${item.content}
           </div>
         </div>`;
+
       list.insertAdjacentHTML("afterend", HTML);
-      formText.value = "";
+      // list要素の直後にメモとして定義したHTMLを挿入している
+      formText.value = ""
+      // 入力フォームに表示されている値をからにしている
     };
+
     e.preventDefault();
+    // submitボタンのCLICKイベントを止めている
   });
 }
 window.addEventListener("load", memo);
+// ページを読み込んだらmemoという関数を実行する
